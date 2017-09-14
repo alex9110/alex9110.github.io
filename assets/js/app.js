cc2b0e200ccfeea7bc2175d16f2e6ba13f5e2b23
alex9110 <cania9110@gmail.com>
2017-09-14 20:36:52 +0300

    x
    x

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,59 @@
 'use strict';
 $(document).ready(function () {
+  ///////////////////////preloader/////////////////////////////
+  $(function () {
+    $('.about-wrapper, .blog-wrapper, .index-wrapper, .works-wrapper, .admin-wrapper').css({'display':'none'});
+    var imgs = [];
+    $.each($('*'), function () {
+      var $this = $(this),
+        background = $this.css('background-image'),
+        img = $this.is('img');
+      if (background != 'none') {
+        var path = background.replace('url("', '').replace('")', '');
+
+        imgs.push(path);
+      }
+      if (img) {
+        path = $this.attr('src');
+        imgs.push(path);
+      }
+    });
+    var percents = 1;
+    for (var i = 0; i < imgs.length; i++) {
+      var image = $('<img>', {
+        attr: {
+          src : imgs[i]
+        }
+      });
+      image.load(function () {
+        setPercents(imgs.length, percents);
+        percents++;
+      });
+      image.error(function () {
+        setPercents(imgs.length, percents);
+        percents++;
+      });
+    }
+    //ЕСЛИ КАРТИНОК НЕТ 
+    if(imgs.length === 0){
+      setPercents(1,1);
+    }
+    function setPercents(total, current) {
+      var percent = Math.ceil(current / total * 100);
+      if (percent >= 100) {
+        $('.about-wrapper, .blog-wrapper, .index-wrapper, .works-wrapper, .admin-wrapper').css({'display':'block'});
+        $('.plate-front').addClass('animate_plate');
+        $('.loader-wrapper').fadeOut(1500, function(){
+          setTimeout(function(){
+            $('.plate-front').removeClass('animate_plate');
+          }, 2000);
+        });
+      }
+      $('.loader__percent').text(percent + '%');
+    }
+  });
+  ///////////////////////preloader/////////////////////////////
+
   ////////////////// плавный скрол ///////////////////////////
   $('a[href^="#"]').click(function(){
     var elementClick = $(this).attr('href');
@@ -410,60 +464,6 @@ $(document).ready(function () {
     });
   })();  
   ///////////////////////////psrallax//////////////////////////
-
-  ///////////////////////preloader/////////////////////////////
-  $(function () {
-    $('.about-wrapper, .blog-wrapper, .index-wrapper, .works-wrapper, .admin-wrapper').css({'display':'none'});
-    var imgs = [];
-    $.each($('*'), function () {
-      var $this = $(this),
-        background = $this.css('background-image'),
-        img = $this.is('img');
-      if (background != 'none') {
-        var path = background.replace('url("', '').replace('")', '');
-
-        imgs.push(path);
-      }
-      if (img) {
-        path = $this.attr('src');
-        imgs.push(path);
-      }
-    });
-    var percents = 1;
-    for (var i = 0; i < imgs.length; i++) {
-      var image = $('<img>', {
-        attr: {
-          src : imgs[i]
-        }
-      });
-      image.load(function () {
-        setPercents(imgs.length, percents);
-        percents++;
-      });
-      image.error(function () {
-        setPercents(imgs.length, percents);
-        percents++;
-      });
-    }
-    //ЕСЛИ КАРТИНОК НЕТ 
-    if(imgs.length === 0){
-      setPercents(1,1);
-    }
-    function setPercents(total, current) {
-      var percent = Math.ceil(current / total * 100);
-      if (percent >= 100) {
-        $('.about-wrapper, .blog-wrapper, .index-wrapper, .works-wrapper, .admin-wrapper').css({'display':'block'});
-        $('.plate-front').addClass('animate_plate');
-        $('.loader-wrapper').fadeOut(1500, function(){
-          setTimeout(function(){
-            $('.plate-front').removeClass('animate_plate');
-          }, 2000);
-        });
-      }
-      $('.loader__percent').text(percent + '%');
-    }
-  });
-  ///////////////////////preloader/////////////////////////////
 
 //////////////////////////skills//////////////////////////
   (function(){
